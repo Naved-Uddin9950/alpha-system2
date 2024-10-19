476f0c4bce83cdcb43563583be7adfe2bb6c953c
naved.uddin <naved.uddin@appsoln.com>
2024-10-19 11:06:57 +0530

    initialized alpha system 2

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+// Stylesheets
+import './index.css';
+import './App.css';
+import 'react-toastify/dist/ReactToastify.css';
+
+// React
+import { Suspense, lazy } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { Spin } from 'antd';
+
+// components
+import App from './App';
+import ErrorPage from './utils/ErrorPage';
+import Home from './components/Home/Home';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<App />} errorElement={<ErrorPage />}>
+        <Route path='' element={<Home />} />
+      </Route>
+    </>
+  )
+);
+
+createRoot(document.getElementById('root')).render(
+  <Suspense fallback={<Spin />}>
+    <RouterProvider router={router} />
+  </Suspense>
+);
